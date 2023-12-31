08901e3db05a67bf99cc337dfdfe95fa2b97447c
Arik Sasmita <ariksasmita@gmail.com>
2023-12-31 14:27:13 +0800

    feat: init header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="header">
+    <div class="title">
+      <h1 class="city-name">Stockholm, Sweden</h1>
+      <h3 class="date">Sun, 31 Dec</h3>
+    </div>
+    <div class="weather">
+      <div class="icon">
+        Icon here
+      </div>
+    </div>
+    <div class="temperature">
+      <h2 class="degree">19<span>&#176;C</span></h2>
+    </div>
+  </div>
+</template>
